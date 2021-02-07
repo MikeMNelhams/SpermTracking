53095e1eb372024e2b67557f03879c71e00f1bcf
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-02-07 12:55:23 +0000

    Create example_vid0_0_frame1.m

---
+++ Scripts/Sperm Head Orientation/example_vid0_0_frame1.m
@@ -0,0 +1,35 @@
+%===============================================================================
+% Read in gray scale demo image.
+folder = "C:\Users\Shane\Desktop\Year 3\Mathematical and Data Modelling\Phase B\MDM3_B\OrientationJ_Data\1st source\Video 0\Frames\"; % Determine where demo folder is (works with all versions).
+baseFileName = 'frame0.jpg';
+% Get the full filename, with path prepended.
+fullFileName = fullfile(folder, baseFileName);
+% Check if file exists.
+if ~exist(fullFileName, 'file')
+	% The file doesn't exist -- didn't find it there in that folder.
+	% Check the entire search path (other folders) for the file by stripping off the folder.
+	fullFileNameOnSearchPath = baseFileName; % No path this time.
+	if ~exist(fullFileNameOnSearchPath, 'file')
+		% Still didn't find it.  Alert user.
+		errorMessage = sprintf('Error: %s does not exist in the search path folders.', fullFileName);
+		uiwait(warndlg(errorMessage));
+		return;
+	end
+end
+rgbImage_raw = imread(fullFileName);
+rgbImage = imcrop(rgbImage_raw,[642 410 64 61]);
+E = edge(rgb2gray(rgbImage),'canny');
+
+% override some default parameters
+params.minMajorAxis = 0;
+params.maxMajorAxis = 100;
+
+% note that the edge (or gradient) image is used
+bestFits = ellipseDetection(E, params);
+fprintf('Output %d best fits.\n', size(bestFits,1));
+angle = mean(bestFits(:,2));
+fprintf('Angle is %d.',angle)
+figure;
+image(rgbImage);
+%ellipse drawing implementation: http://www.mathworks.com/matlabcentral/fileexchange/289 
+ellipse(bestFits(:,3),bestFits(:,4),bestFits(:,5)*pi/180,bestFits(:,1),bestFits(:,2),'k');
