bc6c5ebc128ed2d41e2d88aa880513ce4e271d75
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-02-07 16:12:12 +0000

    Update example_vid0_0_frame1.m

--- Scripts/Sperm Head Orientation/example_vid0_0_frame1.m
+++ Scripts/Sperm Head Orientation/example_vid0_0_frame1.m
@@ -1,7 +1,7 @@
-%===============================================================================
-% Read in gray scale demo image.
+
 folder = "C:\Users\Shane\Desktop\Year 3\Mathematical and Data Modelling\Phase B\MDM3_B\OrientationJ_Data\1st source\Video 0\Frames\"; % Determine where demo folder is (works with all versions).
 baseFileName = 'frame0.jpg';
+first_bbox = [642 410 62 64];
 % Get the full filename, with path prepended.
 fullFileName = fullfile(folder, baseFileName);
 % Check if file exists.
@@ -17,9 +17,8 @@ if ~exist(fullFileName, 'file')
 	end
 end
 rgbImage_raw = imread(fullFileName);
-rgbImage = imcrop(rgbImage_raw,[642 410 64 61]);
-E = edge(rgb2gray(rgbImage),'canny');
-
+rgbImage = imcrop(rgbImage_raw,first_bbox);
+E = edge(rgb2gray(rgbImage),'canny',0.5);
 % override some default parameters
 params.minMajorAxis = 0;
 params.maxMajorAxis = 100;
@@ -27,9 +26,10 @@ params.maxMajorAxis = 100;
 % note that the edge (or gradient) image is used
 bestFits = ellipseDetection(E, params);
 fprintf('Output %d best fits.\n', size(bestFits,1));
-angle = mean(bestFits(:,2));
+angle = mean(bestFits(:,3));
 fprintf('Angle is %d.',angle)
 figure;
 image(rgbImage);
 %ellipse drawing implementation: http://www.mathworks.com/matlabcentral/fileexchange/289 
 ellipse(bestFits(:,3),bestFits(:,4),bestFits(:,5)*pi/180,bestFits(:,1),bestFits(:,2),'k');
+
