703ecd5ff8d8070d04d0253389971f795831fbfc
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-02-09 16:49:42 +0000

    Update example_vid0_0_frame1_sperm1.m

--- Scripts/Sperm Head Orientation/example_vid0_0_frame1_sperm1.m
+++ Scripts/Sperm Head Orientation/example_vid0_0_frame1_sperm1.m
@@ -1,4 +1,6 @@
 
+%===============================================================================
+% Read in gray scale demo image.
 folder = "C:\Users\Shane\Desktop\Year 3\Mathematical and Data Modelling\Phase B\MDM3_B\OrientationJ_Data\1st source\Video 0\Frames\"; % Determine where demo folder is (works with all versions).
 baseFileName = 'frame0.jpg';
 first_bbox = [642 410 62 64];
@@ -20,16 +22,15 @@ rgbImage_raw = imread(fullFileName);
 rgbImage = imcrop(rgbImage_raw,first_bbox);
 E = edge(rgb2gray(rgbImage),'canny',0.5);
 % override some default parameters
-params.minMajorAxis = 0;
-params.maxMajorAxis = 100;
+params.minMajorAxis = 30;
+params.maxMajorAxis = 90;
 
 % note that the edge (or gradient) image is used
 bestFits = ellipseDetection(E, params);
 fprintf('Output %d best fits.\n', size(bestFits,1));
-angle = mean(bestFits(:,3));
+angle = bestFits(:,5);
 fprintf('Angle is %d.',angle)
 figure;
 image(rgbImage);
 %ellipse drawing implementation: http://www.mathworks.com/matlabcentral/fileexchange/289 
 ellipse(bestFits(:,3),bestFits(:,4),bestFits(:,5)*pi/180,bestFits(:,1),bestFits(:,2),'k');
-
