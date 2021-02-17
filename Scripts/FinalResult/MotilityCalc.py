bac52623462a8342346c961fa642ec438efaa40f
Mike Nelhams <61562236+MikeMNelhams@users.noreply.github.com>
2021-02-17 13:51:45 +0000

    Add files via upload

---
+++ Scripts/FinalResult/MotilityCalc.py
@@ -0,0 +1,91 @@
+import tracking_algorithm39 as tr
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def _r_and_t(acceptable_tpG, cover_number, tp='49', verbose=False):
+    r = tr.produce_histogram(bin_count=100, cover=cover_number, cutoff=-2, plot=False, tp=tp,
+                             verbose=verbose)
+    t = tr.num_points(acceptable_tpG, cover=cover_number, tp=tp)
+    if r == -1:
+        r = 0
+    return r, t
+
+
+def calc_rms(tp='49', plot=True, verbose=False, loading_bars=True, mean_line=False):
+    """
+    :param tp: str (int 2 digit tp number)
+    :param plot: bool
+    :param verbose: bool
+    :param loading_bars: bool
+    :param mean_line: bool
+    :return: float (mean RMS value for tp)
+    """
+    n = len(covers)
+    r_all = []
+    total_all = []
+    if loading_bars:
+        if verbose:
+            print('Calculating R for {} covers'.format(n))
+            for i, cover_number in enumerate(covers):
+                r, t = _r_and_t(acceptable_tpG, cover_number, tp, verbose=verbose)
+                r_all.append(r)
+                total_all.append(t)
+                print('Done Cover {} of {}'.format(i+1, n))
+            print('All R: ', r_all)
+            print('All T: ', total_all)
+        else:
+            print('Calculating R for {} covers'.format(n))
+            for i, cover_number in enumerate(covers):
+                r, t = _r_and_t(acceptable_tpG, cover_number, tp, verbose=verbose)
+                r_all.append(r)
+                total_all.append(t)
+                print('Done Cover {} of {}'.format(i+1, n))
+    else:
+        if verbose:
+            for i, cover_number in enumerate(covers):
+                r, t = _r_and_t(acceptable_tpG, cover_number, tp, verbose=verbose)
+                r_all.append(r)
+                total_all.append(t)
+            print('All R: ', r_all)
+            print('All T: ', total_all)
+        else:
+            for i, cover_number in enumerate(covers):
+                r, t = _r_and_t(acceptable_tpG, cover_number, tp, verbose=verbose)
+                r_all.append(r)
+                total_all.append(t)
+
+    mean = np.mean(r_all)
+
+    if plot:
+        # Technically, this is deprecated, but try to stop me >:`)
+        colors = [plt.cm.viridis(r_all[i] / max(r_all)) for i in range(n)]
+
+        fig, ax = plt.subplots()
+        fig.suptitle('RMS distance travelled in 1 frame for tp {}'.format(tp),
+                     fontsize=18)
+        ax.grid()  # Grids are very necessary for bar graphs
+        ax.set_axisbelow(True)  # So the grid goes behind the lines
+        label1 = 'Tp {}'.format(tp)
+        for x_val, y_val, color_val in zip(covers, r_all, colors):
+            ax.bar(x_val, y_val, label=label1, color=color_val)
+
+        if mean_line:
+            xlims = plt.gca().get_xlim()  # X max
+            plt.hlines(mean, xlims[0], xlims[1], colors='red', linestyles='dashed', label='Mean', linewidth=2)
+
+        ax.legend(['Mean'])
+        ax.set_xlabel('Cover Number')
+        ax.set_ylabel('Root Mean Distance travelled by sperms in 1 frame')
+        plt.show()
+
+    return mean
+
+
+# Main
+# calc_rms('49', verbose=True)
+acceptable_tpG = ['49', '57']
+covers = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19']
+
+calc_rms('49', verbose=False, loading_bars=True, mean_line=True)
---
+++ Scripts/FinalResult/test_clusters_tp493d.py
@@ -0,0 +1,163 @@
+import unittest.mock  # Python 3
+
+import tracking_algorithm39 as tp
+
+test_tp = '49'
+
+
+class TestNone(unittest.TestCase):
+    def test_none0(self):
+        tp.run_main(tp=test_tp, cover='00', algorithm='none', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_none1(self):
+        tp.run_main(tp=test_tp, cover='01', algorithm='none', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_none5(self):
+        tp.run_main(tp=test_tp, cover='05', algorithm='none', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_none10(self):
+        tp.run_main(tp=test_tp, cover='10', algorithm='none', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_none13(self):
+        tp.run_main(tp=test_tp, cover='13', algorithm='none', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+class TestDBSCAN(unittest.TestCase):
+    def test_dbscan0(self):
+        tp.run_main(tp=test_tp, cover='00', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_dbscan1(self):
+        tp.run_main(tp=test_tp, cover='01', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_dbscan5(self):
+        tp.run_main(tp=test_tp, cover='05', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_dbscan10(self):
+        tp.run_main(tp=test_tp, cover='10', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_dbscan13(self):
+        tp.run_main(tp=test_tp, cover='13', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+class TestGMM(unittest.TestCase):
+    def test_GMM0(self):
+        tp.run_main(tp=test_tp, cover='00', algorithm='gmm', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_GMM1(self):
+        tp.run_main(tp=test_tp, cover='01', algorithm='gmm', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_GMM5(self):
+        tp.run_main(tp=test_tp, cover='05', algorithm='gmm', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_GMM10(self):
+        tp.run_main(tp=test_tp, cover='10', algorithm='gmm', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_GMM13(self):
+        tp.run_main(tp=test_tp, cover='13', algorithm='gmm', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+class TestMike(unittest.TestCase):
+    def test_Mike0(self):
+        tp.run_main(tp=test_tp, cover='00', algorithm='mike', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Mike1(self):
+        tp.run_main(tp=test_tp, cover='01', algorithm='mike', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Mike5(self):
+        tp.run_main(tp=test_tp, cover='05', algorithm='mike', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Mike10(self):
+        tp.run_main(tp=test_tp, cover='10', algorithm='mike', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Mike13(self):
+        tp.run_main(tp=test_tp, cover='13', algorithm='mike', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+class TestHDBSCAN(unittest.TestCase):
+    def test_HDBSCAN0(self):
+        tp.run_main(tp=test_tp, cover='00', algorithm='hdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HDBSCAN1(self):
+        tp.run_main(tp=test_tp, cover='01', algorithm='hdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HDBSCAN5(self):
+        tp.run_main(tp=test_tp, cover='05', algorithm='hdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HDBSCAN10(self):
+        tp.run_main(tp=test_tp, cover='10', algorithm='hdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HDBSCAN13(self):
+        tp.run_main(tp=test_tp, cover='13', algorithm='hdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+class TestRichard(unittest.TestCase):
+    def test_Richard0(self):
+        tp.run_main(tp=test_tp, cover='00', algorithm='richard-dbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Richard1(self):
+        tp.run_main(tp=test_tp, cover='01', algorithm='richard-dbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Richard5(self):
+        tp.run_main(tp=test_tp, cover='05', algorithm='richard-dbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Richard10(self):
+        tp.run_main(tp=test_tp, cover='10', algorithm='richard-dbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_Richard13(self):
+        tp.run_main(tp=test_tp, cover='13', algorithm='richard-dbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+class TestHTDBSCAN(unittest.TestCase):
+    def test_HTDBSCAN0(self):
+        tp.run_main(tp=test_tp, cover='00', algorithm='htdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HTDBSCAN1(self):
+        tp.run_main(tp=test_tp, cover='01', algorithm='htdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HTDBSCAN5(self):
+        tp.run_main(tp=test_tp, cover='05', algorithm='htdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HTDBSCAN10(self):
+        tp.run_main(tp=test_tp, cover='10', algorithm='htdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+    def test_HTDBSCAN13(self):
+        tp.run_main(tp=test_tp, cover='13', algorithm='htdbscan', plot=True, plot_type='3d')
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
